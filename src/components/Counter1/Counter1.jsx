1eee308f7c0adb84afc7d0410ee1fc7360c1fccd
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-23 06:53:09 +0600

    crete compo..

---
+++ src/components/Counter1/Counter1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter1 = () => {
+  return (
+    <div>
+        Counter1
+
+    </div>
+  )
+}
+
+export default Counter1
---
+++ src/components/Counter2/Counter2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Counter2 = () => {
+  return (
+    <div>
+      Counter2
+      </div>
+  )
+}
+
+export default Counter2
